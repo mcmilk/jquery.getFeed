6b9568cea4ca4fd7066dcc2ea7c6571544847cd1
Tino Reichardt <milky@open-digital-signage.org>
2017-02-03 19:49:14 +0100

    add license to js

--- jquery.getFeed.js
+++ jquery.getFeed.js
@@ -1,10 +1,18 @@
 
 /**
- * jQuery feed parser plugin
+ * Copyright © 2017 Tino Reichardt (milky at Open-Digital-Signage dot org)
  *
- * Copyright (C) 2017 Tino Reichardt (milky at Open-Digital-Signage dot org)
- * - based on code by Jean-François Hovinne
- * - Homepage: https://github.com/mcmilk/jquery.getFeed/
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License Version 2, as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * Based on code by Jean-François Hovinne
+ * Homepage: https://github.com/mcmilk/jquery.getFeed/
  */
 
 jQuery.getFeed = function(options) {
